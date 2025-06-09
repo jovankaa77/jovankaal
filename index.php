fedad5ffa7c942c1dba535543a910ddba7268274
Jovanka alexandro <153190793+jovankaa77@users.noreply.github.com>
2025-06-09 17:01:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
 <div class="container">
         <h1>Hi, Jovanka here!</h1>
         <p>
-            I am Jovanka Alexandro,  a bachelor of computer science student 
+            I am Jovanka Alexandro,  a bachelor of Software engginering student 
         </p>
         <p>
             My passion for software lies with dreaming up ideas and making them come true with elegant interfaces. I take great care in experience, architecture, and code quality of the things I build.
@@ -128,7 +128,7 @@
         </p>
         <br><br>
         <div class="footer">
-            © 2020-PRESENT © Jovanka Alexandro
+            © 2025-PRESENT © Jovanka Alexandro
         </div>
         <br><br>
     </div>
